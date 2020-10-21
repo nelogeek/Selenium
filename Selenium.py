1db0997bd8fb5d5b4e8c0e09f55b7d8b8b7c2174
nelogeek <lokotaevpr@gmail.com>
2020-10-21 21:12:32 +0500

    Initial commit

---
+++ Selenium.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+browser = webdriver.Chrome(
+    '/Users/iell/OneDrive/Рабочий стол/Проекты/Selenium/chromedriver/chromedriver')
+browser.get("https://itproger.com/intensive/python")
